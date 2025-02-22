93de36edbd7dcc1cd279c63f8d7234bc748c8b80
Juan Sebastián Barrera Pulido <47837394+SebasBarrera@users.noreply.github.com>
2025-02-22 18:50:37 +0000

    Pattern AAA has been implemented

--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -4,30 +4,58 @@ import org.junit.jupiter.api.Test;
 class FizzBuzzTest {
 
     @Test
-    void When_NumberIsNotMultipleOf3Or5_Then_ReturnNumberAsString() {
-        // Verifica que un número que no es múltiplo de 3 o 5 se devuelva como string
-        assertEquals("1", FizzBuzz.fizzbuzz(1));
-        assertEquals("7", FizzBuzz.fizzbuzz(7));
+    void When_NumberIsNotMultipleOf3Or5_Then_ReturnNumberAsString()
+    {
+        // Arrange
+        int beginningInput = 1;
+        String expected = "1";
+
+        // Act
+        String actual = FizzBuzz.fizzbuzz(beginningInput);
+
+        // Assert
+        assertEquals(expected, actual);
     }
 
     @Test
-    void When_MultipleOf3_Then_ReturnFizz() {
-        // Verifica que un múltiplo de 3 devuelva "Fizz"
-        assertEquals("Fizz", FizzBuzz.fizzbuzz(3));
-        assertEquals("Fizz", FizzBuzz.fizzbuzz(6));
+    void When_MultipleOf3_Then_ReturnFizz()
+    {
+        // Arrange
+        int beginningInput = 3;
+        String expected = "Fizz";
+
+        // Act
+        String actual = FizzBuzz.fizzbuzz(beginningInput);
+
+        // Assert
+        assertEquals(expected, actual);
     }
 
     @Test
-    void When_MultipleOf5_Then_ReturnBuzz() {
-        // Verifica que un múltiplo de 5 devuelva "Buzz"
-        assertEquals("Buzz", FizzBuzz.fizzbuzz(5));
-        assertEquals("Buzz", FizzBuzz.fizzbuzz(10));
+    void When_MultipleOf5_Then_ReturnBuzz()
+    {
+        // Arrange
+        int beginningInput = 5;
+        String expected = "Buzz";
+
+        // Act
+        String actual = FizzBuzz.fizzbuzz(beginningInput);
+
+        // Assert
+        assertEquals(expected, actual);
     }
 
     @Test
-    void When_MultipleOf3And5_Then_ReturnFizzBuzz() {
-        // Verifica que un múltiplo de 3 y 5 devuelva "FizzBuzz"
-        assertEquals("FizzBuzz", FizzBuzz.fizzbuzz(15));
-        assertEquals("FizzBuzz", FizzBuzz.fizzbuzz(30));
+    void When_MultipleOf3And5_Then_ReturnFizzBuzz()
+    {
+        // Arrange
+        int beginningInput = 15;
+        String expected = "FizzBuzz";
+
+        // Act
+        String actual = FizzBuzz.fizzbuzz(beginningInput);
+
+        // Assert
+        assertEquals(expected, actual);
     }
 }
